40a72c993dc69b2b25949224f1c52651347bac57
devHikary <wrkkarina@gmail.com>
2024-03-22 12:54:37 -0300

    vercel sapper

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 require("./database");
 
-const app = express();
+const app = (module.exports = express());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
